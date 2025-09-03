8db8672dd7b7e74fd6973dee97ecd733371759e5
Jonas Chagas <jonaschagasweb@gmail.com>
2025-09-03 20:27:40 -0300

    feat: adjusts img

--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -20,8 +20,15 @@ export const Works = () => {
       <div className="works__items" ref={containerRef}>
         <Work
           key={crypto.randomUUID()}
-          img="https://sdmntprcentralus.oaiusercontent.com/files/00000000-eaf0-61f5-a2d6-b63c320f4e3b/raw?se=2025-08-27T23%3A21%3A43Z&sp=r&sv=2024-08-04&sr=b&scid=762c4d74-719f-5b90-b7cf-1cc6ed9d98e5&skoid=f28c0102-4d9d-4950-baf0-4a8e5f6cf9d4&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-08-27T20%3A20%3A43Z&ske=2025-08-28T20%3A20%3A43Z&sks=b&skv=2024-08-04&sig=qKphXzXn1812rbsKDLU11l5/yLwFLC7kbKE3VfmtowY%3D"
-          tags={["Flutter", "Dart", "Firebase", "Dio", "Riverpod"]}
+          img="/assets/projects/cineflutter.png"
+          tags={[
+            "Flutter",
+            "Dart",
+            "Firebase",
+            "Dio",
+            "Riverpod",
+            "Clean Architecture",
+          ]}
           title="Cine Flutter"
           description="Aplicativo de visualização de filmes por categorias e gêneros."
           url="https://github.com/JonasGz/cine-flutter"
