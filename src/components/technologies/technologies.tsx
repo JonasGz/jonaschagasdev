d2ddd275827b4a31c7f61c012b226ce43e722370
Jonas Chagas <jonaschagasweb@gmail.com>
2026-06-26 11:41:56 -0300

    feat: replace custom sliders with Swiper
    Migrate works and technologies sliders to Swiper with navigation,
    responsive breakpoints and drag support. Remove unused useScroll hook.

--- src/components/technologies/technologies.tsx
+++ src/components/technologies/technologies.tsx
@@ -1,39 +1,61 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import { Tech } from "./components/tech/tech";
+import "swiper/css";
+import "swiper/css/navigation";
 import "./technologies.scss";
-import { useScroll } from "../../hooks/use-scroll";
+
+const technologies = [
+  { url: "/assets/icons/jsIcon.svg", name: "JavaScript" },
+  { url: "/assets/icons/dartIcon.svg", name: "Dart" },
+  { url: "/assets/icons/tsIcon.svg", name: "TypeScript" },
+  { url: "/assets/icons/reactIcon.svg", name: "React" },
+  { url: "/assets/icons/nextjsIcon.svg", name: "Nextjs" },
+  { url: "/assets/icons/flutterIcon.svg", name: "Flutter" },
+  { url: "/assets/icons/reactIcon.svg", name: "React Native" },
+  { url: "/assets/icons/kotlinIcon.svg", name: "Kotlin" },
+  { url: "/assets/icons/gitIcon.svg", name: "Git" },
+];
 
 export const Technologies = () => {
-  const { containerRef, handleNext, handlePrev } = useScroll({
-    itemsPerView: 4,
-    gap: 10,
-  });
-
   return (
     <div className="technologies">
       <div className="technologies__bg"></div>
 
       <ChevronLeft
         size={28}
-        onClick={handlePrev}
-        className="technologies__btn"
+        className="technologies__btn technologies__btn--prev"
         color="white"
       />
-      <div ref={containerRef} className="technologies__items">
-        <Tech url="/assets/icons/jsIcon.svg" name="JavaScript" />
-        <Tech url="/assets/icons/dartIcon.svg" name="Dart" />
-        <Tech url="/assets/icons/tsIcon.svg" name="TypeScript" />
-        <Tech url="/assets/icons/reactIcon.svg" name="React" />
-        <Tech url="/assets/icons/nextjsIcon.svg" name="Nextjs" />
-        <Tech url="/assets/icons/flutterIcon.svg" name="Flutter" />
-        <Tech url="/assets/icons/reactIcon.svg" name="React Native" />
-        <Tech url="/assets/icons/kotlinIcon.svg" name="Kotlin" />
-        <Tech url="/assets/icons/gitIcon.svg" name="Git" />
-      </div>
+      <Swiper
+        className="technologies__items"
+        modules={[Navigation]}
+        navigation={{
+          prevEl: ".technologies__btn--prev",
+          nextEl: ".technologies__btn--next",
+        }}
+        spaceBetween={10}
+        slidesPerView={4}
+        grabCursor
+        loop
+        centerInsufficientSlides
+        breakpoints={{
+          768: {
+            slidesPerView: "auto",
+            spaceBetween: 16,
+          },
+        }}
+      >
+        {technologies.map((tech) => (
+          <SwiperSlide key={tech.name} className="technologies__slide">
+            <Tech url={tech.url} name={tech.name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <ChevronRight
         size={28}
-        onClick={handleNext}
-        className="technologies__btn"
+        className="technologies__btn technologies__btn--next"
         color="white"
       />
     </div>
--- src/components/works/works.tsx
+++ src/components/works/works.tsx
@@ -1,89 +1,103 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 import { Work } from "./components/work/work";
-import { useScroll } from "../../hooks/use-scroll";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./works.scss";
 
-export const Works = () => {
-  const { containerRef, handleNext, handlePrev } = useScroll({
-    itemsPerView: 1,
-  });
+const works = [
+  {
+    img: "/assets/projects/cineflutter.png",
+    tags: ["Flutter", "Dart", "Firebase", "Dio", "Riverpod", "Clean Architecture"],
+    title: "Cine Flutter",
+    description: "Aplicativo de visualização de filmes por categorias e gêneros.",
+    url: "https://github.com/JonasGz/cine-flutter",
+  },
+  {
+    img: "/assets/projects/petdex.png",
+    tags: ["JavaScript", "HTML", "SCSS", "Firebase"],
+    title: "PetDex",
+    description: "Aplicativo que transforma a experiência de ser tutor de pets.",
+    url: "https://petdex.vercel.app",
+  },
+  {
+    img: "/assets/projects/schedule.png",
+    tags: ["React", "Nextjs", "PWA", "Firebase"],
+    title: "Schedule App",
+    description: "Aplicativo PWA de agendamento de tarefas.",
+    url: "https://schedulepwa.vercel.app",
+  },
+  {
+    img: "/assets/projects/map-marker.png",
+    tags: ["React Native", "RN Maps", "Firebase", "Expo"],
+    title: "Map Marker",
+    description: "Aplicativo para marcações de localizações em mapa.",
+    url: "https://github.com/JonasGz/map-marker-react-native",
+  },
+  {
+    img: "/assets/projects/fuel.png",
+    tags: ["Flutter", "Dart", "Firebase"],
+    title: "Abast Control",
+    description: "Aplicativo para controle de abastecimentos de combustíveis.",
+    url: "https://github.com/JonasGz/abast_control",
+  },
+  {
+    img: "/assets/projects/foodcontrol.png",
+    tags: ["Flutter", "Dart", "Firebase"],
+    title: "Food Control",
+    description: "Aplicativo para controle financeiro de alimentação.",
+    url: "https://github.com/JonasGz/food-finance-control",
+  },
+];
 
+export const Works = () => {
   return (
     <div className="works">
       <div className="works__bg"></div>
       <ChevronLeft
         size={28}
-        onClick={handlePrev}
-        className="works__btn"
+        className="works__btn works__btn--prev"
         color="white"
       />
-      <div className="works__items" ref={containerRef}>
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/cineflutter.png"
-          tags={[
-            "Flutter",
-            "Dart",
-            "Firebase",
-            "Dio",
-            "Riverpod",
-            "Clean Architecture",
-          ]}
-          title="Cine Flutter"
-          description="Aplicativo de visualização de filmes por categorias e gêneros."
-          url="https://github.com/JonasGz/cine-flutter"
-        />
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/petdex.png"
-          tags={["JavaScript", "HTML", "SCSS", "Firebase"]}
-          title="PetDex"
-          description="Aplicativo que transforma a experiência de ser tutor de pets."
-          url="https://petdex.vercel.app"
-        />
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/schedule.png"
-          tags={["React", "Nextjs", "PWA", "Firebase"]}
-          title="Schedule App"
-          description="Aplicativo PWA de agendamento de tarefas."
-          url="https://schedulepwa.vercel.app"
-        />
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/map-marker.png"
-          tags={["React Native", "RN Maps", "Firebase", "Expo"]}
-          title="Map Marker"
-          description="Aplicativo para marcações de localizações em mapa."
-          url="https://github.com/JonasGz/map-marker-react-native"
-        />
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/fuel.png"
-          tags={["Flutter", "Dart", "Firebase"]}
-          title="Abast Control"
-          description="Aplicativo para controle de abastecimentos de combustíveis."
-          url="https://github.com/JonasGz/abast_control"
-        />
-        <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/foodcontrol.png"
-          tags={["Flutter", "Dart", "Firebase"]}
-          title="Food Control"
-          description="Aplicativo para controle financeiro de alimentação."
-          url="https://github.com/JonasGz/food-finance-control"
-        />
-        {/* <Work
-          key={crypto.randomUUID()}
-          img="/assets/projects/loading.png"
-          title="Em breve"
-          description="..."
-        /> */}
-      </div>
+      <Swiper
+        className="works__items"
+        modules={[Navigation, Pagination]}
+        navigation={{
+          prevEl: ".works__btn--prev",
+          nextEl: ".works__btn--next",
+        }}
+        pagination={{ clickable: true }}
+        spaceBetween={24}
+        slidesPerView={1}
+        loop
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 32,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 32,
+          },
+        }}
+      >
+        {works.map((work) => (
+          <SwiperSlide key={work.title} className="works__slide">
+            <Work
+              img={work.img}
+              tags={work.tags}
+              title={work.title}
+              description={work.description}
+              url={work.url}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <ChevronRight
         size={28}
-        onClick={handleNext}
-        className="works__btn"
+        className="works__btn works__btn--next"
         color="white"
       />
     </div>
